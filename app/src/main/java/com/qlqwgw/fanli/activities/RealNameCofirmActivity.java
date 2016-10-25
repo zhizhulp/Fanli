6a6a7d7a70597691b949c6294d18d6877ed9cbc0
lp377762984 <626228379@qq.com>
2016-10-25 19:16:45 +0800

    first commit

---
+++ app/src/main/java/com/qlqwgw/fanli/activities/RealNameCofirmActivity.java
@@ -0,0 +1,15 @@
+package com.qlqwgw.fanli.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qlqwgw.fanli.R;
+
+public class RealNameCofirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_name_cofirm);
+    }
+}
---
+++ app/src/main/java/com/qlqwgw/fanli/activities/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.qlqwgw.fanli.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qlqwgw.fanli.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
