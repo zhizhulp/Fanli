e4653f2cda54c4ce25d004abcaed8439bcbc1613
lp377762984 <626228379@qq.com>
2016-10-28 17:50:19 +0800

    first commit

--- app/src/main/java/com/qlqwgw/fanli/activities/login/LoginActivity.java
+++ app/src/main/java/com/qlqwgw/fanli/activities/login/LoginActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import com.qlqwgw.fanli.R;
 import com.qlqwgw.fanli.activities.base.BaseActivity;
 import com.qlqwgw.fanli.activities.main.MainActivity;
+import com.qlqwgw.fanli.activities.password_loss.PasswordLossActivity;
 import com.qlqwgw.fanli.activities.register.RegisterInputNumberActivity;
 
 public class LoginActivity extends BaseActivity {
@@ -25,6 +26,11 @@ public class LoginActivity extends BaseActivity {
     //登录成功，进入主页
     public void goMain(View view) {
         Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    //进入密码找回页面
+    public void goForgetPassword(View view) {
+        Intent intent=new Intent(this, PasswordLossActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/qlqwgw/fanli/activities/password_loss/PasswordLossWithCodeActivity.java
+++ app/src/main/java/com/qlqwgw/fanli/activities/password_loss/PasswordLossWithCodeActivity.java
@@ -1,10 +1,13 @@
 package com.qlqwgw.fanli.activities.password_loss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.qlqwgw.fanli.R;
 import com.qlqwgw.fanli.activities.base.BaseActivity;
+import com.qlqwgw.fanli.activities.main.MainActivity;
 
 public class PasswordLossWithCodeActivity extends BaseActivity {
 
@@ -12,5 +15,10 @@ public class PasswordLossWithCodeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_loss_with_code);
+    }
+
+    public void goMain3(View view) {
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
