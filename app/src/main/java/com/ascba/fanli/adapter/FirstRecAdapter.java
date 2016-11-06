d56555ef3c23fb4986451261c71b1691e4deff57
lp377762984 <626228379@qq.com>
2016-11-06 18:21:35 +0800

    11.6

---
+++ app/src/main/java/com/ascba/fanli/adapter/FirstRecAdapter.java
@@ -0,0 +1,84 @@
+package com.ascba.fanli.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ascba.fanli.R;
+import com.ascba.fanli.beans.FirstRec;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/11/6.
+ */
+
+public class FirstRecAdapter extends BaseAdapter {
+    private List<FirstRec> mList;
+    private LayoutInflater mInflater;
+
+    public FirstRecAdapter(List<FirstRec> mList, Context mContext) {
+        this.mList = mList;
+        this.mInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if(convertView==null){
+            convertView= mInflater.inflate(R.layout.first_rec_list_item,null);
+            viewHolder=new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+        viewHolder= (ViewHolder) convertView.getTag();
+        FirstRec firstRec = mList.get(position);
+        viewHolder.tvName.setText(firstRec.getName());
+        viewHolder.tvNum.setText(firstRec.getRecNum());
+        viewHolder.tvMoney.setText(firstRec.getMoney());
+        viewHolder.tvTime.setText(firstRec.getTime());
+        return convertView;
+    }
+    private class ViewHolder{
+        TextView tvName;
+        ImageView imType;
+        TextView tvNum;
+        TextView tvMoney;
+        TextView tvTime;
+        public ViewHolder(View root){
+            if(tvName==null){
+                tvName= (TextView) root.findViewById(R.id.first_rec_name);
+            }
+            if(imType==null){
+                imType= (ImageView) root.findViewById(R.id.first_rec_type);
+            }
+            if(tvNum==null){
+                tvNum= (TextView) root.findViewById(R.id.first_rec_num);
+            }
+            if(tvMoney==null){
+                tvMoney= (TextView) root.findViewById(R.id.first_rec_money);
+            }
+            if(tvTime==null){
+                tvTime= (TextView) root.findViewById(R.id.first_rec_time);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/ascba/fanli/fragments/RecommendFragment.java
@@ -0,0 +1,75 @@
+package com.ascba.fanli.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ascba.fanli.R;
+import com.ascba.fanli.activities.main.APSTSViewPager;
+import com.lhh.apst.library.AdvancedPagerSlidingTabStrip;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RecommendFragment extends Fragment {
+    public AdvancedPagerSlidingTabStrip mAPSTS;
+    public APSTSViewPager mVP;
+
+
+    public RecommendFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recommend, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mAPSTS= (AdvancedPagerSlidingTabStrip) view.findViewById(R.id.tabs_recommend);
+        mVP= (APSTSViewPager) view.findViewById(R.id.vp_recommend);
+    }
+
+    public class RecommendItemAdapter extends FragmentStatePagerAdapter{
+
+        public RecommendItemAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if(position >= 0 && position < 2){
+                switch (position){
+                    case  0:
+                        return  "一级推广";
+                    case  1:
+                        return  "二级推广";
+                    default:
+                        break;
+                }
+            }
+            return null;
+        }
+    }
+}
