7b81463439f2920cb5c2ad3f46a3cb3e8c3f56e8
lp377762984 <626228379@qq.com>
2016-12-10 10:39:36 +0800

    12.10

--- app/src/main/java/com/ascba/rebate/activities/TestActivity.java
+++ app/src/main/java/com/ascba/rebate/activities/TestActivity.java
@@ -1,21 +1,8 @@
 package com.ascba.rebate.activities;
 
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.ascba.rebate.R;
 import com.ascba.rebate.activities.base.NetworkBaseActivity;
-import com.ascba.rebate.beans.City;
-import com.ascba.rebate.handlers.CheckThread;
-import com.ascba.rebate.handlers.PhoneHandler;
-import com.yolanda.nohttp.rest.Request;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class TestActivity extends NetworkBaseActivity {
     @Override
