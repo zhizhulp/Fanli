03627cc91b65eca21104dcb9ff4bd0895d1ed32b
TurnTears <lipengb509@gmail.com>
2017-04-11 15:05:39 +0800

    商家联盟

--- app/src/main/java/com/ascba/rebate/activities/BusinessUnionActivity.java
+++ app/src/main/java/com/ascba/rebate/activities/BusinessUnionActivity.java
@@ -2,9 +2,11 @@ package com.ascba.rebate.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ascba.rebate.R;
 import com.ascba.rebate.activities.base.BaseNetWork4Activity;
+import com.ascba.rebate.view.MoneyBar;
 import com.ascba.rebate.view.SuperSwipeRefreshLayout;
 
 /**
@@ -12,15 +14,69 @@ import com.ascba.rebate.view.SuperSwipeRefreshLayout;
  * 我——商家联盟——审核通过
  */
 
-public class BusinessUnionActivity extends BaseNetWork4Activity {
+public class BusinessUnionActivity extends BaseNetWork4Activity implements SuperSwipeRefreshLayout.OnPullRefreshListener, View.OnClickListener {
 
     private Context context;
     private SuperSwipeRefreshLayout refreshLayout;
+    private MoneyBar moneyBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_union);
         context = this;
+        initVIew();
+    }
+
+    private void initVIew() {
+        refreshLayout = (SuperSwipeRefreshLayout) findViewById(R.id.refresh_layout);
+        refreshLayout.setOnPullRefreshListener(this);
+
+        moneyBar = (MoneyBar) findViewById(R.id.moneyBar);
+        moneyBar.setCallBack(new MoneyBar.CallBack() {
+            @Override
+            public void clickImage(View im) {
+
+            }
+
+            @Override
+            public void clickComplete(View tv) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.business_data).setOnClickListener(this);
+        findViewById(R.id.business_account).setOnClickListener(this);
+        findViewById(R.id.business_code).setOnClickListener(this);
+    }
+
+    @Override
+    public void onRefresh() {
+
+    }
+
+    @Override
+    public void onPullDistance(int distance) {
+
+    }
+
+    @Override
+    public void onPullEnable(boolean enable) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.business_code:
+                //立即收款
+                break;
+            case R.id.business_account:
+                //流水记录
+                break;
+            case R.id.business_data:
+                //商家资料
+                break;
+        }
     }
 }
