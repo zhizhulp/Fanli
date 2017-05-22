a170954d70981a949c2ed0509fb34aeda198fc8f
lp377762984 <626228379@qq.com>
2017-05-22 17:30:56 +0800

    搭建拍卖

---
+++ app/src/main/java/com/ascba/rebate/activities/shop/auction/AuctionActivity.java
@@ -0,0 +1,17 @@
+package com.ascba.rebate.activities.shop.auction;
+
+import android.os.Bundle;
+
+import com.ascba.rebate.activities.base.BaseNetActivity;
+
+/**
+ * Created by 李鹏 on 2017/5/22.
+ * 拍卖
+ */
+
+public class AuctionActivity extends BaseNetActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
