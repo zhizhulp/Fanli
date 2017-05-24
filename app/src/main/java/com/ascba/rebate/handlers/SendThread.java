8ad65466f390c55f03899c22456a60f0100d78cc
Administrator <626228379@qq.com>
2017-05-24 11:47:31 +0800

    太多问题修复，具体看word

--- app/src/main/java/com/ascba/rebate/handlers/SendThread.java
+++ app/src/main/java/com/ascba/rebate/handlers/SendThread.java
@@ -1,27 +1,44 @@
 package com.ascba.rebate.handlers;
 
+import android.util.Log;
+
+import com.alibaba.fastjson.JSON;
+import com.ascba.rebate.utils.LogUtils;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 
 public class SendThread extends Thread {
-
+    private static final String TAG = "123";
     private Socket s;
     private String msg;
 
-    public SendThread(Socket s,String msg) {
-        this.s = s;
+    public SendThread(String msg) {
         this.msg = msg;
     }
 
     public void run() {
-        OutputStream out ;
         try {
-            out = s.getOutputStream();
-            out.write(msg.getBytes());
-            out.flush();
+            s= new Socket("118.190.2.177", 8283);
+            LogUtils.PrintLog("123","close-->"+s.isClosed()+",connect-->"+s.isConnected());
+            OutputStream out = s.getOutputStream();
+            out.write(((String) JSON.toJSONString(msg)).getBytes());
+            Log.d(TAG, "发送消息: "+msg);
         } catch (IOException e) {
             e.printStackTrace();
+            Log.d(TAG, "连接异常："+e.getMessage());
+        }
+    }
+    //关闭连接
+    public void disConnect() {
+        if (s != null && !s.isClosed()) {
+            try {
+                s.close();
+            } catch (IOException e) {
+                throw new RuntimeException("关闭异常:" + e.getMessage());
+            }
+            s = null;
         }
     }
 }
