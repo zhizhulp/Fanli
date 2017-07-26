f60b6f25f930d5e7eea150e1d89a3c9db4f39b78
Administrator <626228379@qq.com>
2017-07-26 15:26:57 +0800

    处理登陆失败情况

--- app/src/main/java/com/ascba/rebate/activities/base/BaseNetActivity1.java
+++ app/src/main/java/com/ascba/rebate/activities/base/BaseNetActivity1.java
@@ -36,7 +36,7 @@ public class BaseNetActivity1 extends BaseActivityNet {
             }
             mhandle200Data(what, jObj, dataObj, message);
 
-        } else if ( status == 3) {//需要登陆
+        } else if ( status == 3 || status == 4 || status == 5) {//需要登陆
             //jpush
             AppConfig.getInstance().putBoolean("jpush_set_tag_success",false);
             AppConfig.getInstance().putBoolean("jpush_set_alias_success",false);
@@ -52,7 +52,7 @@ public class BaseNetActivity1 extends BaseActivityNet {
             AppConfig.getInstance().putLong("expiring_time", -2000);
             AppConfig.getInstance().putInt("uuid", -1000);
             startActivityForResult(intent, REQUEST_LOGIN);
-        } else if (status == 1 || status == 2 || status == 4 || status == 5) {//缺少sign参数
+        } else if (status == 1 || status == 2 ) {//缺少sign参数
             showToast(message);
         } else if (status == 404) {
             mhandle404(what, dataObj, message);
