27083703c14eef270ae515f103302296ba44b7ba
Administrator <626228379@qq.com>
2017-07-26 08:50:57 +0800

    竞拍更新问题

--- app/src/main/java/com/ascba/rebate/activities/TestActivity.java
+++ app/src/main/java/com/ascba/rebate/activities/TestActivity.java
@@ -14,23 +14,10 @@ import com.ascba.rebate.utils.PsdUtils;
 
 public class TestActivity extends BaseNetActivity {
 
-    private EditText editText;
-    private Button button;
-    private TextView textView;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        editText = (EditText) findViewById(R.id.edit);
-        button = (Button) findViewById(R.id.button);
-        textView = (TextView) findViewById(R.id.text);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = PsdUtils.encryptPsd(editText.getText().toString());
-                textView.setText(text + "\n" + PsdUtils.getPayPsd(editText.getText().toString()));
-            }
-        });
+
     }
 }
